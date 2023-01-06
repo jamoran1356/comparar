0a05aaa2f4dfadfe89fb277a510dd9b2b5e4dbe8
jamoran1356 <jamoran1356@gmail.com>
2023-01-06 14:58:34 -0400

    Update

--- index.php
+++ index.php
@@ -4,12 +4,14 @@ You are given two strings - pattern and source. The first string pattern contain
 
 Let's say that pattern matches a substring source[l..r] of source if the following three conditions are met:
 
-(x)they have equal length,
+they have equal length,
 for each 0 in pattern the corresponding letter in the substring is a vowel,
 for each 1 in pattern the corresponding letter is a consonant.
 Your task is to calculate the number of substrings of source that match pattern.
 
 Note: In this task we define the vowels as 'a', 'e', 'i', 'o', 'u', and 'y'. All other letters are consonants.*/
+
+
 function compare($pattern, $source){
     if(strlen($pattern)==strlen($source)){
         //Las cadenas tienen la misma longitud
